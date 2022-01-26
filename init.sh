3c499bc7dbecd62e486992a2af3b8686b5814bde
yushuai <ken200930@126.com>
2022-01-26 14:22:47 +0800

    add

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 # git clone https://github.com/AElfProject/aelf-devops.git
 
-set -e
+# set -e
 
 CONFIG_FILE="/etc/zabbix/zabbix_agentd.conf"
 
@@ -12,6 +12,7 @@ CONF_NUM=$(grep -Ev "^#|^$" ${CONFIG_FILE}| grep -c "Include=${FOLDER_DIR}")
 
 [ "${CONF_NUM}" -eq 0 ] && echo "Include=${FOLDER_DIR}/*.conf" >> ${CONFIG_FILE}
 
+chmod -R 777 "${FOLDER_DIR}"/sqlite3/
 
-#chmod -R 777 sqlite3/
-#/etc/init.d/zabbix-agent restart
+/etc/init.d/zabbix-agent restart
+
