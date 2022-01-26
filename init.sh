32e4aa8a809f667a5e4e9556eeba87954574ba04
yushuai <ken200930@126.com>
2022-01-26 14:34:07 +0800

    up

--- init.sh
+++ init.sh
@@ -10,6 +10,10 @@ FOLDER_DIR=$(cd "$(dirname "$0")";pwd)
 
 CONF_NUM=$(grep -Ev "^#|^$" ${CONFIG_FILE}| grep -c "Include=${FOLDER_DIR}")
 
+pip3 install -r "${FOLDER_DIR}"/requestments.txt
+
+[ $? -ne 0 ] && exit 0
+
 [ "${CONF_NUM}" -eq 0 ] && echo "Include=${FOLDER_DIR}/*.conf" >> ${CONFIG_FILE}
 
 chmod -R 777 "${FOLDER_DIR}"/sqlite3/
