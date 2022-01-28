5601935a80988ec4bc88e025fdbf49f96a8969e9
yushuai <ken200930@126.com>
2022-01-28 17:36:22 +0800

    up

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # aelf-devops
 
+## Install
 ```shell
-bash <(curl -s "https://raw.githubusercontent.com/AElfProject/aelf-devops/main/init.sh" )
+bash <(curl -s "https://raw.githubusercontent.com/AElfProject/aelf-devops/main/init.sh install")
+```
+### verify
+output node height
+```shell
+python3 /etc/zabbix/zabbix_agentd.d/aelf-devops/aelf_node_status_info.py block_height
+```
+
+Output node synchronization delay time, in seconds
+```shell
+python3 /etc/zabbix/zabbix_agentd.d/aelf-devops/aelf_node_status_info.py sync_status
+```
+
+Output BP_Produced_Blocks_Normal is normal
+```shell
+python3 /etc/zabbix/zabbix_agentd.d/aelf-devops/bp_node_produced_blocks.py
+```
+
+
+## Update
+```shell
+bash <(curl -s "https://raw.githubusercontent.com/AElfProject/aelf-devops/main/init.sh update")
+
+```
+
+
+## Uninstall
+```shell
+bash <(curl -s "https://raw.githubusercontent.com/AElfProject/aelf-devops/main/init.sh uninstall")
+
 ```
--- init.sh
+++ init.sh
@@ -1,28 +1,63 @@
 #!/bin/bash
 
-# cd /etc/zabbix/zabbix_agentd.d/ && git clone https://github.com/AElfProject/aelf-devops.git && cd aelf-devops/
-
 # set -e
+
 GITHUB_URL="https://github.com/AElfProject/aelf-devops.git"
 #FOLDER_DIR=$(cd "$(dirname "$0")";pwd)
 FOLDER_DIR="/etc/zabbix/zabbix_agentd.d/aelf-devops"
 CONFIG_FILE="/etc/zabbix/zabbix_agentd.conf"
-
 CONF_NUM=$(grep -Ev "^#|^$" ${CONFIG_FILE}| grep -c "Include=${FOLDER_DIR}")
-PIP_PACKAGE=$(dpkg -l | grep -c "python3-pip")  # Check if the python3-pip package is installed on the Ubuntu system
 
-#[ ! -d ${FOLDER_DIR} ] && echo "${FOLDER_DIR} directory does not exist" && exit 1;
 
-git clone ${GITHUB_URL} ${FOLDER_DIR}
+install_scripts() {
+  git clone ${GITHUB_URL} ${FOLDER_DIR}
+  PIP_PACKAGE=$(dpkg -l | grep -c "python3-pip")
+  [ "${PIP_PACKAGE}" -eq 0 ] && apt install -y python3-pip
+  pip3 install -r "${FOLDER_DIR}"/requestments.txt
+  [ $? -ne 0 ] && exit 0
+  [ "${CONF_NUM}" -eq 0 ] && echo "Include=${FOLDER_DIR}/zabbix-user-parameter.conf" >> ${CONFIG_FILE}
+  chmod -R 777 "${FOLDER_DIR}"/sqlite3/
+}
 
-[ "${PIP_PACKAGE}" -eq 0 ] && apt install -y python3-pip
 
-pip3 install -r "${FOLDER_DIR}"/requestments.txt
-[ $? -ne 0 ] && exit 0
+uninstall_scripts() {
+  [ "${CONF_NUM}" -ne 0 ] && sed -i '/aelf-devops/d' ${CONFIG_FILE}
+  date=$(date "+%Y%m%d-%H%M")
+  mv ${FOLDER_DIR} /tmp/aelf-devops-"${date}"
+}
 
-[ "${CONF_NUM}" -eq 0 ] && echo "Include=${FOLDER_DIR}/zabbix-user-parameter.conf" >> ${CONFIG_FILE}
 
-chmod -R 777 "${FOLDER_DIR}"/sqlite3/
+update_scripts() {
+  cd ${FOLDER_DIR} && { git pull && [ $? -ne 0 ]; exit 0; }
+  FILE_NUM=$(grep -Ev "^#|^$" ${CONFIG_FILE}| grep "zabbix-user-parameter.conf"|grep -c "Include=${FOLDER_DIR}")
 
-/etc/init.d/zabbix-agent restart
+  if [ "${CONF_NUM}" -ne 0 ] && [ "${FILE_NUM}" -eq 0 ]; then
+    sed -i '/aelf-devops/d' ${CONFIG_FILE}
+    echo "Include=${FOLDER_DIR}/zabbix-user-parameter.conf" >> ${CONFIG_FILE}
+  fi;
+}
+
+
+restart_server() {
+  /etc/init.d/zabbix-agent restart
+}
+
+
+case "$1" in
+    install)
+        install_scripts
+        restart_server
+        ;;
+    uninstall)
+        uninstall_scripts
+        restart_server
+        ;;
+    update)
+        update_scripts
+        restart_server
+        ;;
+    *)
+        echo "Usage: $0 {install|uninstall|update}"
+        exit 2
+esac
 
