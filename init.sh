6f7126631fa4583f47efd1bb7e641d6ca36ab08b
yushuai <ken200930@126.com>
2022-01-28 15:46:06 +0800

    up

--- init.sh
+++ init.sh
@@ -3,19 +3,21 @@
 # cd /etc/zabbix/zabbix_agentd.d/ && git clone https://github.com/AElfProject/aelf-devops.git && cd aelf-devops/
 
 # set -e
-
+GITHUB_URL="https://github.com/AElfProject/aelf-devops.git"
+#FOLDER_DIR=$(cd "$(dirname "$0")";pwd)
+FOLDER_DIR="/etc/zabbix/zabbix_agentd.d"
 CONFIG_FILE="/etc/zabbix/zabbix_agentd.conf"
 
-FOLDER_DIR=$(cd "$(dirname "$0")";pwd)
-
 CONF_NUM=$(grep -Ev "^#|^$" ${CONFIG_FILE}| grep -c "Include=${FOLDER_DIR}")
-
 PIP_PACKAGE=$(dpkg -l | grep -c "python3-pip")  # 检查 Ubuntu 系统是否安装 python3-pip 软件包
+
+[ ! -d ${FOLDER_DIR} ] && echo "${FOLDER_DIR} 目录不存在" && exit 1;
+
+git clone ${GITHUB_URL} ${FOLDER_DIR}/aelf-devops
 
 [ "${PIP_PACKAGE}" -eq 0 ] && apt install -y python3-pip
 
 pip3 install -r "${FOLDER_DIR}"/requestments.txt
-
 [ $? -ne 0 ] && exit 0
 
 [ "${CONF_NUM}" -eq 0 ] && echo "Include=${FOLDER_DIR}/*.conf" >> ${CONFIG_FILE}
