b90710eff8ebc10d313506cc6cfa48c8a5cc8b78
yushuai <ken200930@126.com>
2022-01-28 18:42:10 +0800

    up

--- aelf_node_status_info.py
+++ aelf_node_status_info.py
@@ -25,22 +25,21 @@ def node_block_height_utctime(height_num, ip="127.0.0.1"):
 
 if __name__ == '__main__':
     # ip = "18.185.93.36"
-    num = node_block_height()
-    if sys.argv[1] == "block_height":
-        print(num)
-    elif sys.argv[1] == "sync_status":
-        block_utc_time = node_block_height_utctime(num)
-        local_time = time.localtime(time.time())
-        local_time_stamp = int(time.mktime(local_time))
+    try:
+        num = node_block_height()
+        if sys.argv[1] == "block_height":
+            print(num)
+        elif sys.argv[1] == "sync_status":
+            block_utc_time = node_block_height_utctime(num)
+            local_time = time.localtime(time.time())
+            local_time_stamp = int(time.mktime(local_time))
+            block_utc_time_str = f"{block_utc_time.split('T')[0]} {block_utc_time.split('T')[1].split('.')[0]}"
+            block_utc_time_array = time.strptime(block_utc_time_str, "%Y-%m-%d %H:%M:%S")
+            block_utc_time_stamp = int(time.mktime(block_utc_time_array))
+            time_diff = local_time_stamp - block_utc_time_stamp
+            print(time_diff)
+        else:
+            pass
 
-        block_utc_time_str = f"{block_utc_time.split('T')[0]} {block_utc_time.split('T')[1].split('.')[0]}"
-        block_utc_time_array = time.strptime(block_utc_time_str, "%Y-%m-%d %H:%M:%S")
-        block_utc_time_stamp = int(time.mktime(block_utc_time_array))
-        time_diff = local_time_stamp - block_utc_time_stamp
-        # if int(time_diff) > int(sys.argv[2]):
-        #     print(time_diff)
-        # else:
-        #     print(0)
-        print(time_diff)
-    else:
-        pass
+    except requests.exceptions.ConnectionError as e:
+        print(e)
