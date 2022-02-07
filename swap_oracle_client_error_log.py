667469578374f3431b9f99597f4ff74fbfaa101e
yushuai <ken200930@126.com>
2022-02-07 17:10:17 +0800

    up

--- swap_oracle_client_error_log.py
+++ swap_oracle_client_error_log.py
@@ -15,21 +15,19 @@ class SwapOracleClientError(LoadConf):
         error_log_list = []
         for logs in self.swap_oracle_client_log:
             if os.path.exists(logs):
-                # print(logs)
                 # command = f"grep -r '\[ERR\]' {logs} | grep '{old_1_min}'"
-                command = f"grep -r '\[ERR\]' {logs}"
-                # error_log = subprocess.getoutput(command)
+                command = f"grep -r '\[ERR\]' {logs} | grep -E '2022-02-05 01:5|2021-09-08 17:43|2021-09-13 12:22|2021-09-12 04:5'"
                 error_log = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                              encoding="utf-8")
                 output, error = error_log.communicate()
-                # print(len(output.split(os.linesep)))
-                # print(output.split(os.linesep))
                 if len(output.split(os.linesep)) > 1:
                     error_log_list = error_log_list + output.split(os.linesep)
         return error_log_list
 
 
-# grep -r "\[ERR\]" /opt/swap-oracle-client*/Logs/ | grep "2022-02-05 01:5"
 if __name__ == '__main__':
     swap_oracle_client = SwapOracleClientError()
-    print(swap_oracle_client.check_error_log())
+    error_list = swap_oracle_client.check_error_log()
+    for line in error_list:
+        print(line)
+
